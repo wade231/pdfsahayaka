bd01f0c7058f46cd2f183e21c03645a1c48e23c1
wade231 <122469077+wade231@users.noreply.github.com>
2023-10-19 18:37:27 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -137,10 +137,9 @@ def list_to_str(k):
     else:
         return ' '.join(f'{elem}, ' for elem in k)
 
-__repo__ = "https://github.com/MrMKN/PROFESSOR-BOT"
 __version__ = "PROFESSOR-BOT ᴠ4.5.0"
 __license__ = "GNU GENERAL PUBLIC LICENSE V2"
-__copyright__ = "Copyright (C) 2023-present MrMKN <https://github.com/MrMKN>"
+__copyright__ = "Copyright (C) 2023-PDF-පුස්තකාලය <https://t.me/pdfpusthakalaya1>"
 
 async def search_gagala(text):
     usr_agent = {
